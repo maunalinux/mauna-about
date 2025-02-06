e741096b2808c99bd38ffbdaa6eef64d5ed82573
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2025-02-05 21:21:45 -0300

    feat: cli mode

--- src/main.py
+++ src/main.py
@@ -2,18 +2,45 @@
 
 import sys
 import gi
+import cli
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import GLib, Gio, Gtk
 
 from MainWindow import MainWindow
 
+
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="top.mauna.about", flags=Gio.ApplicationFlags.NON_UNIQUE, **kwargs)
+        super().__init__(
+            *args,
+            application_id="top.mauna.about",
+            flags=Gio.ApplicationFlags.NON_UNIQUE | Gio.ApplicationFlags(8),
+            **kwargs
+        )
         self.window = None
-    
+        self.add_main_option(
+            "nogui",
+            ord("n"),
+            GLib.OptionFlags(0),
+            GLib.OptionArg.NONE,  # Adjusted for boolean flag (no argument expected)
+            "No GUI mode",
+            None,
+        )
+        GLib.set_prgname("top.mauna.about")
+
     def do_activate(self):
-        self.window = MainWindow(self)
+        if not self.args or "nogui" not in self.args:
+            self.window = MainWindow(self)
+        else:
+            cli.CLI()
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        options = options.end().unpack()
+        self.args = options
+        self.activate()
+        return 0
 
 
 if __name__ == "__main__":
