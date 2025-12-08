1dc8fd349f1d094480ac87a55b84cd2cfc36f4c0
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2025-12-08 18:40:23 -0300

    feat: added DiskManager.py

---
+++ src/util/DiskManager.py
@@ -0,0 +1,45 @@
+import subprocess
+import json
+
+
+disks = []
+
+
+def get_disks():
+    global disks
+    if disks:
+        return disks
+
+    p = subprocess.run(
+        ["lsblk", "-bdJ", "-o", "MODEL,SIZE,TRAN,TYPE,VENDOR,SERIAL"],
+        capture_output=True,
+        text=True,
+    )
+    if p.returncode == 0:
+        output = json.loads(p.stdout)
+        for d in output["blockdevices"]:
+            if d["type"] != "disk":
+                continue
+
+            size = int(d["size"]) / 1000 / 1000 / 1000
+            size_name = "GB"
+            if size > 1000:
+                size = size / 1000
+                size_name = "TB"
+                if size > 1000:
+                    size = size / 1000
+                    size_name = "PB"
+
+            size = int(size)
+
+            disk = {
+                "model": d["model"],
+                "serial": d["serial"],  # private
+                "vendor": d["vendor"],
+                "type": d["tran"],
+                "size": f"{size} {size_name}",
+            }
+
+            disks.append(disk)
+
+    return disks
