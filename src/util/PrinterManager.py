133bad91788b9f6503dd7bc6fc9c7827c3aa7378
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2025-12-08 15:38:44 -0300

    feat: added printer detection support

---
+++ src/util/PrinterManager.py
@@ -0,0 +1,22 @@
+import cups
+
+
+def get_printers():
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+
+    printers_list = []
+    for p in printers:
+        printer_obj = {
+            "name": p,
+            "device-uri": printers[p]["device-uri"],
+            "printer-uri": printers[p]["printer-uri-supported"],
+            "info": printers[p]["printer-make-and-model"],
+        }
+
+        printers_list.append(printer_obj)
+
+    return printers_list
+
+
+print(get_printers())
