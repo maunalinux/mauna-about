523c245e23d32131b669cefd23ff32f40cdce241
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2025-12-10 17:22:06 -0300

    fix hardcoded actions.py path

--- src/util/ComputerManager.py
+++ src/util/ComputerManager.py
@@ -116,9 +116,16 @@ class ComputerManager:
         # Is Live USB?
         self.computer_info["live_boot"] = self.is_live_boot()
 
+        # Oem Available
+        self.computer_info["oem"] = os.path.isfile("/sys/firmware/acpi/tables/MSDM")
+
+        # Deep sleep mode support
+        with open("/sys/power/mem_sleep", "r") as f:
+            self.computer_info["mem_sleep_support"] = "deep" in f.read()
+
         # ACPI:
         p = subprocess.run(
-            ["pkexec", "/usr/share/mauna/mauna-about/src/Actions.py", "acpi"]
+            ["pkexec", os.path.dirname(os.path.abspath(__file__))+"/../Actions.py", "acpi"]
         )
         self.computer_info["is_acpi_supported"] = p.returncode == 0
 
