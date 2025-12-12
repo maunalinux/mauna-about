d46fbc1718fbd93c22ebc353435ef01d89678e92
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2025-12-12 15:47:50 -0300

    update version information from version file

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -65,6 +65,15 @@ class MainWindow:
         task = Gio.Task.new(callback=self.on_read_hardware_info_finish)
         task.run_in_thread(self.read_hardware_info)
 
+        version = '0.0.0'
+        try:
+            with open(os.path.dirname(os.path.abspath(__file__)) + "/__version__") as f:
+                version = f.readline().strip()
+        except:
+            print("Failed to fetch verion")
+
+        self.ui_about_dialog.set_version(version)
+
         self.control_args()
 
         # Show Screen:
