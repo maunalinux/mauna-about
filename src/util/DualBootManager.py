a2903ca059e63d1bdaa446b24f053abf2195093a
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2026-04-08 15:14:45 -0300

    fix: remove quotes in distro name

--- src/util/DualBootManager.py
+++ src/util/DualBootManager.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import json
+
+
 def list_parts():
     ret = []
     for disk in os.listdir("/sys/block"):
@@ -10,6 +12,7 @@ def list_parts():
             ret.append(part)
     return ret
 
+
 def get_root_part():
     with open("/proc/mounts", "r") as f:
         for line in f.read().split("\n"):
@@ -17,30 +20,34 @@ def get_root_part():
                 return line.split(" ")[0]
     return None
 
+
 win_table = {
-    '10.': '10',
-    '6.4': '10',
-    '6.3': '8.1',
-    '6.2': '8',
-    '6.1': '7',
-    '6.0': 'Vista',
-    '5.2': 'XP Pro x64',
-    '5.1': 'XP',
-    '5.0': '2000',
-    '4.9': 'ME',
-    '4.1': '98',
-    '4.0': '95',
+    "10.": "10",
+    "6.4": "10",
+    "6.3": "8.1",
+    "6.2": "8",
+    "6.1": "7",
+    "6.0": "Vista",
+    "5.2": "XP Pro x64",
+    "5.1": "XP",
+    "5.0": "2000",
+    "4.9": "ME",
+    "4.1": "98",
+    "4.0": "95",
 }
+
+
 def get_windows_version():
     if not os.path.isdir("/run/winroot/Windows/servicing/Version"):
         return ""
     for item in os.listdir("/run/winroot/Windows/servicing/Version"):
-        if len(item) <3:
+        if len(item) < 3:
             continue
         item = item[:3]
         if item in win_table.keys():
             return win_table[item]
     return ""
+
 
 def get_dualboot_oses():
     dualboot = {}
@@ -49,24 +56,30 @@ def get_dualboot_oses():
     for part in list_parts():
         if f"/dev/{part}" == root_part:
             continue
-        sp = subprocess.run(["mount", "-o", "defaults,ro", f"/dev/{part}", "/run/winroot"], capture_output=True)
+        sp = subprocess.run(
+            ["mount", "-o", "defaults,ro", f"/dev/{part}", "/run/winroot"],
+            capture_output=True,
+        )
         if 0 == sp.returncode:
             # Windows
             if os.path.exists("/run/winroot/Windows/System32/ntoskrnl.exe"):
                 dualboot[part] = "Windows " + get_windows_version()
             # Mac OS X
-            if os.path.exists("/run/winroot/System/Library/CoreServices/SystemVersion.plist"):
+            if os.path.exists(
+                "/run/winroot/System/Library/CoreServices/SystemVersion.plist"
+            ):
                 dualboot[part] = "Mac OS X"
             # Linux
             if os.path.exists("/run/winroot/etc/os-release"):
                 with open("/run/winroot/etc/os-release", "r") as f:
                     for line in f.read().split("\n"):
                         if line.startswith("NAME="):
-                            dualboot[part] = line[5:]
+                            dualboot[part] = line[6:-1]
             os.system("umount -lf /run/winroot")
 
     os.rmdir("/run/winroot")
     return json.dumps(dualboot)
+
 
 if __name__ == "__main__":
     print(get_dualboot_oses())
